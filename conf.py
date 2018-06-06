65b291d1f6138190ddacd5ba57d68a6ede2f2a8c
Joana Chavez <chavezlucero88@gmail.com>
2018-06-06 22:06:47 +0200

    changing config

--- conf.py
+++ conf.py
@@ -28,18 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinxcontrib.httpdomain',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain',
-        'doc_extensions',
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -109,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -117,16 +106,13 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 html_theme_options = {
     'sticky_navigation': True,
-    'style_external_links': True,
+    'collapsiblesidebar':True,
     'includehidden': True,
-    'collapse_navigation': True,
-    'prev_next_buttons_location': 'bottom',
     'navigation_depth': 4,
-    'includehidden' : False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes",]
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
